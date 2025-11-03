26fd02eb54737eda2c0b80c0061773f37e733cf1
Vishal Singh <singh287686@gmail.com>
2025-11-03 19:05:20 +0000

    learnt about borrowing and references in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hello");
+    let s2 = &s1;
+
+    println!("{}", s1);
+    println!("{}", s2);
+
+    let my_string = String::from("initial string");
+    takes_ownership(&my_string);
+
+    println!("{}", my_string);
+
+    // Doing hanky-panky while borrowing
+
+    let mut str = String::from("Hello");
+
+    update_fn(&mut str);
+
+    println!("{}", str);
+
+
+}
+
+fn takes_ownership(some_string: &String) {
+    println!("{}", some_string);
+}
+
+fn update_fn(s: &mut String) {
+    s.push_str(" World")
 }
