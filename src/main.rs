39fbe453b5d2a06bdd97a76c784e9db5c63e1dd3
Vishal Singh <singh287686@gmail.com>
2025-11-03 10:01:57 +0000

    learnt about ownership how the ownership passes from one to another

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    let s1 = String::from("hello");
+
+    println!("{}", s1);
+
+    let s2 = s1;
+
+    // println!("{}", s1);  cannot be compiled coz ownership moved to s2
+
+    println!("{}", s2);
+
 }
