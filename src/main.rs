612a4eda4153f9cb981c8ce072591347060134a9
Vishal Singh <singh287686@gmail.com>
2025-11-04 11:12:10 +0000

    Learnt about struct in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+struct User {
+    active : bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Rect {
+    width : i32,
+    height: i32,
+}
+
+impl Rect {
+    fn area(&self) -> i32 {
+        return self.width * self.height;
+    }
+
+    fn perimeter(&self) -> i32 {
+        return 2 * (self.width + self.height);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User{
+        active : true,
+        username : String::from("Vishal"),
+        email: String::from("singh287686@gmail@gmail.com"),
+        sign_in_count : 1,
+    };
+
+    println!("Status : active : {}, name : {}", user1.active, user1.username);
+
+
+    let rect = Rect{
+        width : 20,
+        height : 15,
+    };
+
+    println!("The Area of rectangle is {}", rect.area());
+    println!("The Perimeter of rectangle is {}", rect.perimeter());
+
 }
